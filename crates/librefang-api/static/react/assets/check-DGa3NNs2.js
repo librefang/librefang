52a6dd187be80fd7c2ad52fabafe24789f942065
Evan Hu <e-hu@unext.jp>
2026-04-03 20:50:44 +0900

    chore: bump version to v2026.4.3-beta11

--- crates/librefang-api/static/react/assets/check-DGa3NNs2.js
+++
@@ -1,1 +0,0 @@
-import{P as c}from"./index-C9HVQc31.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
---
+++ crates/librefang-api/static/react/assets/loader-circle-DO49IUt7.js
@@ -0,0 +1,1 @@
+import{P as e}from"./index-hxyv275T.js";const o=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],a=e("loader-circle",o);export{a as L};
